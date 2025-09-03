8a91e3ecce6846e826578b1f5267ceee858a456c
Jaycey <jayceybrugge23@gmail.com>
2025-09-03 11:57:06 +0200

    added search input and heading text, added shadcn ui dependency

---
+++ components/SearchInput.tsx
@@ -0,0 +1,16 @@
+import { Input } from "@/components/ui/input"
+import { Search } from "lucide-react"
+
+export default function SearchInput() {
+  return (
+    <div className="relative w-full">
+      <Search className="absolute left-2 top-1/2 -translate-y-1/2 text-gray-500 h-5 w-5" />
+
+      <Input
+        type="search"
+        placeholder="Search for a song..."
+        className="pl-9 h-12"
+      />
+    </div>
+  )
+}
