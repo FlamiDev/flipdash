bc61eae7a7ba6568beca4d68795acd5d5f6fc135
Jaycey <jayceybrugge23@gmail.com>
2025-09-04 10:03:10 +0200

    refactor searchinput code

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -6,11 +6,7 @@ export default function SearchInput() {
     <div className="relative w-full">
       <Search className="absolute left-2 top-1/2 -translate-y-1/2 text-gray-500 h-5 w-5" />
 
-      <Input
-        type="search"
-        placeholder="Search for a song..."
-        className="pl-9 h-12"
-      />
+      <Input type="search" placeholder="Search for a song..." className="pl-9 h-12" />
     </div>
   )
 }
